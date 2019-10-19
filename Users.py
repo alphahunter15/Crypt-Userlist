58997788a8798865e5e25314ce27964f2440ceeb
alphahunter15 <56774154+alphahunter15@users.noreply.github.com>
2019-10-19 14:27:09 -0700

    Create Users.py

---
+++ Users.py
@@ -0,0 +1,18 @@
+password = input('Password:')
+if password == 'communist':
+  print('accepted')
+  print('Welcome, Isaiah')
+elif password == 'charlie':
+  print('accepted')
+  print('Welcome, Charlie')
+elif password == 'cookie':
+  print('accepted')
+  print('Welcome, Mom')
+elif password == 'bmf':
+  print('accepted')
+  print('Welcome, Dad')
+elif password == 'llama':
+  print('accepted')
+  print('Welcome, Grace')
+else:
+  print('Go away')
