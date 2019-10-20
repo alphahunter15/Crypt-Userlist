773a326a835ec33d2801d9df4cd70975d76581fb
alphahunter15 <56774154+alphahunter15@users.noreply.github.com>
2019-10-20 11:50:40 -0700

    Update Users.py

--- Users.py
+++ Users.py
@@ -1,18 +1,75 @@
+import datetime
+date_object = datetime.date.today()
+print(date_object)
+
+def rev_str(s):
+  return s[::-1]
+
+import reverse_isaiah
+import reverse_angel
+import reverse_charlie
+
 password = input('Password:')
+
 if password == 'communist':
-  print('accepted')
-  print('Welcome, Isaiah')
+  q = input('Future:')
+  if q == 'MIT':
+    print('password accepted')
+    print('Welcome, Isaiah')
+    command = input('read or log:')
+    if command == 'log':
+      i = open("isaiah_log.py", "a+")
+      i.write(rev_str(input("ISAIAH'S LOG:")))
+      i.write('\n')
+      i.close()
+    elif command == 'read':
+      print(exec('reverse_isaiah'))
+    else:
+      print('unexpected command, try again')
+  else:
+    print('leave, you sit on a throne of lies.')
+  print('\n')
+
 elif password == 'charlie':
-  print('accepted')
-  print('Welcome, Charlie')
-elif password == 'cookie':
-  print('accepted')
-  print('Welcome, Mom')
-elif password == 'bmf':
-  print('accepted')
-  print('Welcome, Dad')
-elif password == 'llama':
-  print('accepted')
-  print('Welcome, Grace')
+  q = input('what game do we play on Fridays (in one word):')
+  if q == ['smash', 'Smash']:
+    print('password accepted')
+    print('Welcome, Charlie')
+    command = input('read or log:')
+    if command == 'log':
+      c = open("charlie_log.py", "a+")
+      c.write(rev_str(input("CHARLIE'S LOG:")))
+      c.write('\n')
+      c.close()
+    elif command == 'read':
+      exec(reverse_charlie)
+    else:
+      print('unexpected command, try again')
+  else:
+    print('Imposter! You sit on a trone of lies')
+  print('\n')
+
+
+elif password == 'love of my life':
+  q = input('what movie did we watch first together:')
+  if q == 'Up':
+    print('password accepted')
+    print('infinity - 2 = infinity')
+    print('Welcome, Lilly')
+    command = input('read or log:')
+    if command == 'log':
+      l = open("angel_log.py", "a+")
+      l.write(rev_str(input("ANGEL'S LOG:")))
+      l.write('\n')
+      l.close()
+    elif command == 'read':
+      exec(reverse_angel)
+    else:
+      print('unexpected command, try again')
+  else:
+    print('imposter! you sit on a throne of lies')
+  print('\n')
+
 else:
+  print('password rejected')
   print('Go away')
