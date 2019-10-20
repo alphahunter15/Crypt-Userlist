ef7f01db06816297d30fa765ee642c5e400b455c
alphahunter15 <56774154+alphahunter15@users.noreply.github.com>
2019-10-20 12:33:06 -0700

    Update Users.py

--- Users.py
+++ Users.py
@@ -1,3 +1,7 @@
+# So, Im trying to make it so you can add in your credentials then either read or write in your specific "log", but when I do 
+# the read function, it doesnt pritn anything, you can check the module reverse_isaiah and isaiah_log for mistakes. Also, 
+# isaiah_log is supposed o be reversed so theoretically, people cant read the log or write the log without the credentials.
+
 import datetime
 date_object = datetime.date.today()
 print(date_object)
@@ -16,8 +20,8 @@ if password == 'communist':
   if q == 'MIT':
     print('password accepted')
     print('Welcome, Isaiah')
-    command = input('read or log:')
-    if command == 'log':
+    command = input('read or write:')
+    if command == 'write':
       i = open("isaiah_log.py", "a+")
       i.write(rev_str(input("ISAIAH'S LOG:")))
       i.write('\n')
@@ -35,8 +39,8 @@ elif password == 'charlie':
   if q == ['smash', 'Smash']:
     print('password accepted')
     print('Welcome, Charlie')
-    command = input('read or log:')
-    if command == 'log':
+    command = input('read or write:')
+    if command == 'write':
       c = open("charlie_log.py", "a+")
       c.write(rev_str(input("CHARLIE'S LOG:")))
       c.write('\n')
@@ -56,8 +60,8 @@ elif password == 'love of my life':
     print('password accepted')
     print('infinity - 2 = infinity')
     print('Welcome, Lilly')
-    command = input('read or log:')
-    if command == 'log':
+    command = input('read or write:')
+    if command == 'write':
       l = open("angel_log.py", "a+")
       l.write(rev_str(input("ANGEL'S LOG:")))
       l.write('\n')
